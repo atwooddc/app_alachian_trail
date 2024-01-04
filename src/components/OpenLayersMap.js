a8e0afa191c0f01335818987023c38fb1c93d6a2
David Atwood <70248461+atwooddc@users.noreply.github.com>
2024-01-04 12:02:59 -0800

    added section pop ups

--- src/components/OpenLayersMap.js
+++ src/components/OpenLayersMap.js
@@ -3,14 +3,32 @@ import { fromLonLat } from "ol/proj";
 import GeoJSON from "ol/format/GeoJSON";
 import "ol/ol.css";
 
-import { RMap, RLayerVector, RStyle } from "rlayers";
+import { RMap, RLayerVector, RStyle, ROverlay, RFeature } from "rlayers";
 import RLayerStadia from "rlayers/layer/RLayerStadia";
 import { MapContext } from "../context/MapContext";
-import { fetchDataForDay } from "../utils/dataUtils";
+
+import { Paper, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
 const center = fromLonLat([-76.17, 41.76]);
 
+const useStyles = makeStyles((theme) => ({
+    popUp: {
+        minWidth: "105px",
+        width: "flex",
+        padding: "10px 10px",
+        boxShadow: "0 2px 5px rgba(0, 0, 0, 0.2)",
+        alignContent: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        zIndex: 1000,
+        fontFamily: "Futura",
+        color: "#11151C",
+    },
+}));
+
 const OpenLayersMap = ({ setData }) => {
+    const classes = useStyles();
     const [currentSection, setCurrentSection] = useState(null);
     const [currentStop, setCurrentStop] = useState(null);
 
@@ -46,13 +64,32 @@ const OpenLayersMap = ({ setData }) => {
                         duration: 1000,
                         maxZoom: 11,
                     });
-
                     setData(e.target.getProperties());
                 }}
             >
                 <RStyle.RStyle>
                     <RStyle.RStroke color="#8A4942" width={2.5} />
                 </RStyle.RStyle>
+            </RLayerVector>
+
+            <RLayerVector zIndex={10}>
+                {currentSection ? (
+                    <div>
+                        <RFeature geometry={currentSection.getGeometry()}>
+                            <ROverlay className="overlay" autoPosition={true}>
+                                <Paper className={classes.popUp}>
+                                    <Typography
+                                        variant="p"
+                                        fontWeight="bold"
+                                        align="center"
+                                    >
+                                        Day {currentSection.get("day")}
+                                    </Typography>
+                                </Paper>
+                            </ROverlay>
+                        </RFeature>
+                    </div>
+                ) : null}
             </RLayerVector>
 
             <RLayerVector
@@ -76,6 +113,20 @@ const OpenLayersMap = ({ setData }) => {
                     </RStyle.RCircle>
                 </RStyle.RStyle>
             </RLayerVector>
+
+            {/* shelter pop ups
+            note: need to revise names of shelters in geojson */}
+            {/* <RLayerVector zIndex={10}>
+                {currentStop ? (
+                    <div>
+                        <RFeature geometry={currentStop.getGeometry()}>
+                            <ROverlay className="overlay" autoPosition={true}>
+                                <strong>{currentStop.get("Name")}</strong>
+                            </ROverlay>
+                        </RFeature>
+                    </div>
+                ) : null}
+            </RLayerVector> */}
         </RMap>
     );
 };
