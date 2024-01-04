ac1fe192c42bb48d1c43316c27521ce93315e2c2
David Atwood <70248461+atwooddc@users.noreply.github.com>
2024-01-03 21:54:01 -0800

    improved header images

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,29 +5,38 @@ const Header = () => {
     return (
         <div className="map-header">
             <img
-                src={require("../img/beige.jpg")}
-                alt="beige"
-                className="header-image"
-            />
-            <img
-                src={require("../img/first_day_pic.jpg")}
+                src={require("../img/first_day_pic_zoomed_in.png")}
                 alt="first day"
                 className="header-image"
             />
             <div color="#11151C">
-                <h1>Putting the A.T. in Atwood</h1>
+                <h1>
+                    Putting the &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; in
+                    Atwood
+                </h1>
                 <p>Glenn's 1981 Appalachian Trail trek from GA to ME</p>
             </div>
             <img
-                src={require("../img/blue_sticker.jpg")}
-                alt="blue journal"
+                src={require("../img/stack_of_journals.png")}
+                alt="blue planning"
                 className="header-image"
             />
             <img
-                src={require("../img/at_archive_1.jpg")}
-                alt="at archive 1"
+                src={require("../img/at_logo_from_blue_notebook.png")}
+                alt="blue journal"
                 className="header-image"
             />
+            {/* <img
+                src={require("../img/beige.jpg")}
+                alt="beige"
+                className="header-image"
+            />
+
+            <img
+                src={require("../img/itinerary.jpg")}
+                alt="itinerary"
+                className="header-image"
+            /> */}
         </div>
     );
 };
