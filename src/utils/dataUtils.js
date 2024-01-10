272ff51d10c871ba61eb28500ab6a0b6762ff4d0
David Atwood <70248461+atwooddc@users.noreply.github.com>
2024-01-09 16:31:09 -0800

    deleted old files

--- src/utils/dataUtils.js
+++
@@ -1,9 +0,0 @@
-export const fetchDataForDay = async (day, setData) => {
-    try {
-        const response = await fetch(`[Your AWS API Endpoint]?day=${day}`);
-        const data = await response.json();
-        setData(data);
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
-};
