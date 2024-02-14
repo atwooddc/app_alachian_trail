ab215fddb236456626b060811077e065a128d181
David Atwood <70248461+atwooddc@users.noreply.github.com>
2024-02-13 22:32:16 -0500

    header mobile friendly test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,22 @@
 import React from "react";
 import "./Header.css";
+import atLogo from "../img/at_logo_b&w.png";
+import Stack from "@mui/material/Stack";
 
 const Header = () => {
     return (
-        <div className="map-header">
-            <img
-                src={require("../img/at_logo_b&w.png")}
-                alt="at-logo"
-                className="header-image"
-            />
+        <div className="map-header desktop">
             <div color="#11151C">
-                <h1>
-                    Putting the &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; in
-                    Atwood
-                </h1>
+                <Stack
+                    direction="row"
+                    className="Stack"
+                    spacing={1.5}
+                    marginTop={1}
+                >
+                    <h1>Putting the</h1>
+                    <img src={atLogo} alt="at-logo" className="header-image" />
+                    <h1>in Atwood</h1>
+                </Stack>
                 <p>Exploring Glenn's 1981 trek from Georgia to Maine</p>
             </div>
         </div>
